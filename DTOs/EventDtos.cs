b955c8370ddae1cc58854211832fe123610f6430
Mert <mfindicak@gmail.com>
2020-01-11 15:43:03 +0300

    Created Models and Dtos.

---
+++ DTOs/EventDtos.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace net_core_bootcamp_b1_mert.DTOs
+{
+    public class EventAddDto
+    {
+        [Required, MaxLength(250)]
+        public string Name { get; set; }
+        [Required]
+        public DateTime StartDate { get; set; }
+        [Required]
+        public DateTime FinishDate { get; set; }
+        [Required]
+        public string Location { get; set; }
+        [Required]
+        public bool isFree { get; set; }
+        public double Price { get; set; }
+        public string Subject { get; set; }
+        public string Desc { get; set; }
+    }
+
+    public class EventUpdateDto : EventAddDto
+    {
+        [Required]
+        public Guid Id { get; set; }
+    }
+
+    public class EventGetDto
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime FinishDate { get; set; }
+        public string Location { get; set; }
+        public bool isFree { get; set; }
+        public double Price { get; set; }
+        public string Subject { get; set; }
+        public string Desc { get; set; }
+    }
+}
---
+++ Models/Event.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace net_core_bootcamp_b1_mert.Models
+{
+    public class Event
+    {
+        public Guid Id { get; set; }
+        public bool isDeleted { get; set; }
+        public DateTime CreatedAt { get; set; }
+        [Required, MaxLength(250)]
+        public string Name { get; set; }
+        [Required]
+        public DateTime StartDate { get; set; }
+        [Required]
+        public DateTime FinishDate { get; set; }
+        [Required]
+        public string Location { get; set; }
+        [Required]
+        public bool isFree { get; set; }
+        public double Price { get; set; }
+        public string Subject { get; set; }
+        public string Desc { get; set; }
+    }
+}
