1a748bf78fe84ce3bdf407b9c91a6e60a53cd7c6
Mert <mfindicak@gmail.com>
2020-01-18 01:36:30 +0300

    Add homework book model and dtos.

---
+++ DTOs/HWBookDtos.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace net_core_bootcamp_b1_mert.DTOs
+{
+    public class HWBookAddDto
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Author { get; set; }
+        public string Publisher { get; set; }
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+        [Required]
+        public double Price { get; set; }
+    }
+    public class HWBookUpdateDto:HWBookAddDto
+    {
+        public Guid Id { get; set; }
+    }
+    public class HWBookGetDto
+    {
+        public Guid? Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public string Publisher { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ Models/HWBook.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace net_core_bootcamp_b1_mert.Models
+{
+    public class HWBook
+    {
+        public Guid? Id { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime CreatedAt { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Author { get; set; }
+        public string Publisher { get; set; }
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+        [Required]
+        public double Price { get; set; }
+    }
+}
